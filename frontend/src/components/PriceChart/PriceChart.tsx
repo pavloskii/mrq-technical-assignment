0ed2bd8d2cbcb3238df985ee578d9c0a9eda5e3f
pavloskii <pavlo.igor@gmail.com>
2025-04-02 17:47:07 +0200

    Fixed unintended behaviour in the Price Chart

--- frontend/src/components/PriceChart/PriceChart.tsx
+++ frontend/src/components/PriceChart/PriceChart.tsx
@@ -13,7 +13,11 @@ const PriceChart = ({ symbolId }: PriceChartProps) => {
   const dispatch = useAppDispatch();
   useEffect(() => {
     if (symbolId) {
-      dispatch(fetchPriceHistory(symbolId));
+      const promise = dispatch(fetchPriceHistory(symbolId));
+
+      return () => {
+        promise.abort();
+      };
     }
   }, [dispatch, symbolId]);
 
