7a44f0f4247543f2555e687108aa3b0622e5e66a
pavloskii <pavlo.igor@gmail.com>
2025-04-03 23:59:30 +0200

    Performance card price formatting to USD

--- frontend/src/components/PerformanceCard/src/VolumeLabel.tsx
+++ frontend/src/components/PerformanceCard/src/VolumeLabel.tsx
@@ -3,6 +3,7 @@ import './performanceInfo.css';
 import { ReactComponent as UpArrow } from '@/assets/up-arrow.svg';
 import { ReactComponent as DownArrow } from '@/assets/down-arrow.svg';
 import ListItem from '@/components/ListItem';
+import formatPrice from '@/utils/formatPrice';
 
 type TrendLabelProps = {
   volume: number;
@@ -11,7 +12,7 @@ type TrendLabelProps = {
 
 const VolumeLabel = ({ volume, change }: TrendLabelProps) => {
   const arrow = change > 1 ? <UpArrow /> : <DownArrow />;
-  return <ListItem Icon={arrow} label={volume.toString()} />;
+  return <ListItem Icon={arrow} label={formatPrice(volume)} />;
 };
 
 export default memo(VolumeLabel);
